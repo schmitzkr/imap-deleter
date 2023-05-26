acc33a327eded070856291c31a079c61bc7f1383
schmitzkr <github@mael.is>
2023-05-26 13:34:03 -0700

    adding date conditions and logging

--- imap-deleter.py
+++ imap-deleter.py
@@ -1,12 +1,28 @@
 #delete messages from a folder over 30 days old
 # https://stackoverflow.com/questions/3180891/imap-how-to-delete-messages
 
+#!/bin/python
+
 import imaplib
 import config
-box = imaplib.IMAP4_SSL('mail.mael.is', 993)
+import datetime
+import logging
+
+current_date = datetime.date.today()
+d = 30
+datedays = (current_date-datetime.timedelta(days=d)).strftime("%d-%b-%Y")
+
+box = imaplib.IMAP4_SSL(config.host, 993)
 box.login(config.username, config.password)
 box.select("INBOX.delete-after-30-days")
-typ, data = box.search(None, 'ALL')
+typ, data = box.search(None, '(BEFORE {0})'.format(datedays))
+
+count = len(data[0].split())
+
+logging.basicConfig(level=logging.DEBUG, filename="logfile", filemode="a+",
+                        format="%(asctime)-15s %(levelname)-8s %(message)s")
+logging.info('%d messages deleted', count)
+
 for num in data[0].split():
     box.store(num, '+FLAGS', '\\Deleted')
 
